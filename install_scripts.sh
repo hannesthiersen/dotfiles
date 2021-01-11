ef60ca45bd1f56b88635aa6293f099e3e87b392a
Hannes Thiersen <hannesthiersen@gmail.com>
2021-01-12 01:16:22 +0200

    vim-plug plugin manager install script

---
+++ install_scripts.sh
@@ -0,0 +1,2 @@
+# Install vim-plug plugin manager
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
